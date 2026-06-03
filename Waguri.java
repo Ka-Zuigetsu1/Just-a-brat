55184703bae25264ce9006e6518bc076101563b4
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-03 21:59:25 +0530

    First commit

---
+++ Waguri.java
@@ -0,0 +1,49 @@
+package Mine;
+
+import java.util.Scanner;
+
+public class Waguri {
+    public static void main(String[] args) {
+        System.out.println("Yokoso");
+        System.out.println("Who is the best Girl in Animeverse?");
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+//        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+//        int sum = a+b;
+//        System.out.println("Sum of these numbers is");
+//        System.out.println(sum);
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+//        String str = sc.nextLine();
+//        System.out.println(str);
+//        System.out.println("Marks in Physics");
+//        int a = sc.nextInt();
+//        System.out.println("Marks in Chemistry");
+//        int b = sc.nextInt();
+//        System.out.println("Marks in Maths");
+//        int c = sc.nextInt();
+//        System.out.println("Marks in Additional Subject");
+//        int d = sc.nextInt();
+//        System.out.println("Marks in English");
+//        int e = sc.nextInt();
+//        int g = 500;
+//        int h = 100;
+//        double Percentage = ((double)(a+b+c+d+e)/g)*h;
+//        System.out.printf("Your Boards Percentage is %.2f", Percentage);
+        String str = sc.nextLine();
+        System.out.println("Alright but if you guess the wrong answer you are gay");
+        String gg = sc.nextLine();
+        System.out.println("yeah");
+        String Waggu = sc.nextLine();
+        if (Waggu.equals("Waguri")) {
+            System.out.println("You Passed");
+        } else{
+            System.out.println("You are gay");
+        }
+
+
+    }
+}
