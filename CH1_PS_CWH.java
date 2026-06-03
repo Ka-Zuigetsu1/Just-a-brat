5fd467c55556fec92a8aa991a0d2d11aa0f5f4ec
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-03 22:16:02 +0530

    Secodnd

---
+++ CH1_PS_CWH.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class CH1_PS_CWH {
+
+//   public static void main(String[] args) {
+//      System.out.println("Adding three numbers");
+//      Scanner sc = new Scanner(System.in);
+//      System.out.println("enter first number");
+//      int a = sc.nextInt();
+//      System.out.println("Enter Second Number");
+//      int b = sc.nextInt();
+//      System.out.println("Enter Third Number");
+//      int c = sc.nextInt();
+//      int sum = a+b+c;
+//      System.out.println(sum);
+//      System.out.println("Here is your Final result");
+//   }
+//   QUESTION 2
+//   public static void main(String[] args) {
+//      int Subject1 = 85;
+//      int Subject2 = 90;
+//      int Subject3 = 60;
+//      float CGPA = ((float)Subject1+Subject2+Subject3)/30;
+//      System.out.println(CGPA);
+//   }
+}
+//  QUESTION 3
+
+//public static void main(String[] args) {
+//   System.out.println("Hey, What is your name?");
+//   Scanner name = new Scanner(System.in);
+//   String a = name.next();
+//   System.out.println(a + ",Welcome abode");
+//}
+
+//   Question 4
+//public static void main(String[] args) {
+//   System.out.println("Enter Distance in km");
+//   Scanner km = new Scanner(System.in);
+//   float a = km.nextFloat();
+//   float b = 0.62f;
+//   float Conversion = (a)*b;
+//   System.out.println(Conversion);
+//
+//}
+//QUESTION 5
+public static void main(String[] args) {
+    System.out.println("Enter an Integer");
+    Scanner sc = new Scanner(System.in);
+    System.out.println(sc.hasNextInt());
+}
