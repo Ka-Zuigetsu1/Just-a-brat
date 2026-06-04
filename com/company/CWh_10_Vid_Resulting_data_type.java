d513cc597329efe81de92e36fe9de744eadf2721
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-04 11:26:11 +0530

    Third commit

---
+++ com/company/CWh_10_Vid_Resulting_data_type.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class CWh_10_Vid_Resulting_data_type {
+    public static void main(String[] args) {
+       /* byte x = 5;
+        short z = 4;
+        float a = 6.54f + x;
+        int b = 654 + 6;
+        char g = 'A';
+        byte h = 2;
+        short y = 255;
+        System.out.println(a);*/
+        // Increment and Decrement Operator
+//        int a = 67;
+//        System.out.println(a++);
+//        System.out.println(a);
+//        System.out.println(++a);
+//        System.out.println(a++);
+//        System.out.println(a);
+        int y = 7;
+        int x = ++y*8;
+        System.out.println(x);
+        char a = 'B';
+        System.out.println(++a);
+    }
+
+}
---
+++ com/company/Video09_cwh_ch02_PrenAssociativity.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Video09_cwh_ch02_PrenAssociativity {
+    public static void main(String[] args) {
+//        int b = 6*7-100/2;
+//        System.out.println(b);
+        // Precedence & Associativity
+        // Highest precedence goes * and / so they are evaluated first as per left to right associativity
+
+        // Quick Quiz
+//        int a = 33;
+//        int b = 0;
+//        int c = 0;
+//        int d = (b*b-4*a*c)/(2*a);
+//        System.out.println(d);
+//        int a = 55/0;
+//        System.out.println(a);
+        // Same rule as Maths cannot divide by 0, here is no Infinity!
+
+    }
+}
