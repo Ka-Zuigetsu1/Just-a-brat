1fadd1e7b748c7889f0c441410241d68109b7465
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-04 22:05:00 +0530

    Third commit

---
+++ Problems/Grade_encryption.java
@@ -0,0 +1,17 @@
+package Problems;
+
+public class Grade_encryption {
+    public static void main() {
+        /* int grade = 'B';
+        grade = (char) (grade + 8);
+        System.out.println(grade);
+
+        // Decrypt
+        grade = (int)(grade-8);
+        System.out.println(grade); */
+
+        int a = 'B';
+        System.out.println(a);
+    }
+
+}
