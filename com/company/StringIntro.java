ce0402b57ead2789e6520598d8a7915afdd402b1
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-05 10:42:35 +0530

    Fourth commit

---
+++ com/company/StringIntro.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class StringIntro {
+   /* public static void main(String[] args) {
+        String name = new String("Jhinshi");
+        System.out.print("Waga namae wa: ");
+        System.out.println(name);
+    }*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Zoro lost again?");
+        String a = sc.nextLine();
+        System.out.println("Yk, I recently saw zoro in best anime girls of 2025 list, mann that guy is everywhere");
+        String b = sc.nextLine();
+    }
+
+}
