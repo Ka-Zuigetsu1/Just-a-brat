ce199857c52615ce7153d612d509b53da29dc6e5
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-05 21:35:22 +0530

    Fourth commit

--- com/company/StringIntro.java
+++ com/company/StringIntro.java
@@ -6,12 +6,19 @@ public class StringIntro {
         System.out.print("Waga namae wa: ");
         System.out.println(name);
     }*/
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Zoro lost again?");
         String a = sc.nextLine();
         System.out.println("Yk, I recently saw zoro in best anime girls of 2025 list, mann that guy is everywhere");
         String b = sc.nextLine();
-    }
+    }*/
+   /* public static void main(String[] args) {
+        String a = new String("My name is Ka Zuigetsu - The Moon Prince");
+        int b = 99;
+        float t = 3.2994f;
+        System.out.printf("This is %d and the other atrociously weird number is %9.3f", b, t);
+    }*/
+
 
 }
---
+++ com/company/String_Methods_Vid14.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.util.Scanner;
+public class String_Methods_Vid14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // STRING INDEXING IS AS FOLLOWS 0123 for ANOU
+        String baka = new String("Anou");
+//        int a = baka.length();
+//        System.out.println(a);
+        String b = baka.toUpperCase();
+       // System.out.println(b);
+
+
+        String BAKA = new String("         Anou        ");
+        String trimmed = BAKA.trim();
+       // System.out.println(trimmed);
+
+
+       // System.out.println(baka.substring(2,8));
+       // System.out.println(baka.replace('A','a'));
+
+
+        /* boolean a = sc.hasNextInt();
+        System.out.println(a);*/
+
+
+       // System.out.println(baka.startsWith("an"));
+       // System.out.println(baka.endsWith("ou"));
+
+
+       // String a = sc.nextLine();
+       // System.out.println(a.endsWith("Fool!"));
+       // System.out.println(baka.equals("Anou"));
+       // System.out.println(baka.equalsIgnoreCase("anOU"));
+        System.out.println("\"My name is Ka Zuigetsu - The Moon Prince\"");
+
+
+    }
+}
