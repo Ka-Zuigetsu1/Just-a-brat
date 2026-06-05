13468260e5a7fe3b275eb1b7c382a8206284e1e9
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-05 21:37:22 +0530

    Save local changes before pulling

---
+++ com/company/CWH_Ch02_LogicalOP.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class CWH_Ch02_LogicalOP {
+//        public static void main(String[] args) {
+//        int a = 9;
+//        a +=5;
+//        % operator will give you the remainder
+//        System.out.println(a);
+//        System.out.println(6>6);
+//        System.out.println(6422>66 && 767>100);
+//    }
+}
---
+++ com/company/Java.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("It's my first code lets go");
+        byte age = 64;
+        int age2 = 66;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double f23 = 5.932D;
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(f23);
+        System.out.println(a);
+
+    }
+
+}
