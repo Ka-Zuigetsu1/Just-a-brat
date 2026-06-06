974e644b8ce69061f6c3bccfdfd3f59c881a979a
Ka-Zuigetsu1 <amit2958@gmail.com>
2026-06-06 10:55:46 +0530

    Fifth commit

---
+++ com/company/If_Else_Vid16n17.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.Scanner;
+
+public class If_Else_Vid16n17 {
+//    static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Hola!!");
+//        System.out.println("\"Praise The Fool!\" who is the fool?");
+//        String a = input.nextLine();
+//        if (a.equals("The King of yellow and black who wields good luck")){
+//            System.out.println("Klein Moretti");
+//        } else {
+//            System.out.println("Read LOTM");
+//        }
+//    }
+    /*static void main(String[] args) {
+        int a = 18;
+        if (a>=18){
+            System.out.println("ok");
+        } else {
+            System.out.println("nah");
+        }
+    }*/
+//    static void main(String[] args) {
+//        int a = 10;
+//        int b = 31;
+//        System.out.println(a>11 || b>10);
+//    }
+
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = false;
+        if (a||b == false) {
+            System.out.println("Y");
+        } else {
+            System.out.println("N");
+        }
+//        System.out.println(!a);
+//        System.out.println(!b && !a);
+    }
+}
+
+
