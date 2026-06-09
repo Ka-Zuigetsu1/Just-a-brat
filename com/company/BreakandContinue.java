3840c1f9293b55fb2e4a81f6ca9c0e29b85cfcb2
Ka Zuigetsu <amit2958@gmail.com>
2026-06-10 01:36:18 +0530

    Seventh commit

---
+++ com/company/BreakandContinue.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class BreakandContinue {
+    static void main(String[] args) {
+        // Break and continue using loops
+        int a = 0;
+//        for (int i = 0; i <5; i++) {
+//            System.out.println(i);
+//            System.out.println("Re9");
+//            if (i==2) {
+//                System.out.println("End");
+//                break;
+//            }
+//        }
+        while (a<50) {
+            System.out.println(++a);
+            System.out.println("Requiem");
+
+//        }
+//        do {
+//            System.out.println(++a);
+//            if (a==50){
+//                System.out.println("Bye");
+//                break;
+//            }
+//        } while (a<100);
+
+    }
+        System.out.println("Bye Bye");}}
